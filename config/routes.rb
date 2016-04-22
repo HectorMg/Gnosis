4d46bb3dadca51379483634fb8260e35507cffe2
Hector Mg <hectormgwake@gmail.com>
2016-04-22 00:11:19 -0500

    Add static pages routing

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  root            'static_pages#home'
+  get 'home'    => 'static_pages#home'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'help'    => 'static_pages#help'
+  
+end
